a353958de07fcf743c42d770ae80a817d3f0484b
user89986 <167529891+user89986@users.noreply.github.com>
2024-06-07 23:52:28 +0300

    create

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GeoLibrary
+{
+    public class Class1
+    {
+        public class GeoJSONData
+        {
+            [JsonProperty("type")]
+            public string Type { get; set; }
+
+            [JsonProperty("geometry")]
+            public GeometryData Geometry { get; set; }
+        }
+
+        public class GeometryData
+        {
+            [JsonProperty("type")]
+            public string Type { get; set; }
+
+            [JsonProperty("coordinates")]
+            public object Coordinates { get; set; }
+        }
+
+        public static GeoJSONData LoadGeoJSON(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"GeoJSON file not found at {filePath}");
+            }
+
+            string json = File.ReadAllText(filePath);
+            var geojsonData = JsonConvert.DeserializeObject<GeoJSONData>(json);
+            return geojsonData;
+        }
+
+        public static void SaveGeoJSON(string filePath, GeoJSONData data)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static string GetGeometryType(string geoJsonData)
+        {
+            JObject json = JObject.Parse(geoJsonData);
+            return json["geometry"]["type"].ToString();
+        }
+
+        public static object GetCoordinates(GeoJSONData data)
+        {
+            return data.Geometry.Coordinates;
+        }
+
+        public static double CalculateArea(GeoJSONData data)
+        {
+            if (data.Geometry.Type != "Polygon")
+            {
+                throw new ArgumentException("Data does not represent a Polygon type");
+            }
+
+            var coordinates = ((JArray)data.Geometry.Coordinates).ToObject<List<List<double>>>();
+
+            double area = 0;
+            for (int i = 0; i < coordinates.Count; i++)
+            {
+                int j = (i + 1) % coordinates.Count;
+                area += coordinates[i][0] * coordinates[j][1];
+                area -= coordinates[i][1] * coordinates[j][0];
+            }
+
+            return Math.Abs(area) / 2;
+        }
+
+        public static double CalculateDistance(GeoJSONData data1, GeoJSONData data2)
+        {
+            if (data1.Geometry.Type != "Point" || data2.Geometry.Type != "Point")
+            {
+                throw new ArgumentException("Data does not represent Point type");
+            }
+
+            var coordinates1 = ((JArray)data1.Geometry.Coordinates).ToObject<List<double>>();
+            var coordinates2 = ((JArray)data2.Geometry.Coordinates).ToObject<List<double>>();
+
+            double x1 = coordinates1[0];
+            double y1 = coordinates1[1];
+            double x2 = coordinates2[0];
+            double y2 = coordinates2[1];
+
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using GeoLibrary;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+               
+                string filePath = "F:\\Проект\\ConsoleApp1\\ConsoleApp1\\bin\\Debug\\file"; 
+
+               
+                Class1.GeoJSONData geoJsonData = Class1.LoadGeoJSON(filePath);
+                Console.WriteLine("Загружены данные GeoJSON:");
+                PrintGeoJSONData(geoJsonData);
+
+                // Вычисление и вывод площади, если тип геометрии - Polygon
+                if (geoJsonData.Geometry.Type == "Polygon")
+                {
+                    double area = Class1.CalculateArea(geoJsonData);
+                    Console.WriteLine($"Площадь полигона: {area}");
+                }
+
+                // Если тип геометрии - Point, демонстрация вычисления расстояния до той же точки
+                if (geoJsonData.Geometry.Type == "Point")
+                {
+                    double distance = Class1.CalculateDistance(geoJsonData, geoJsonData);
+                    Console.WriteLine($"Расстояние до той же точки: {distance} (должно быть 0)");
+                }
+
+             
+                string saveFilePath = "E:\\Проект\\ConsoleApp1\\ConsoleApp1\\bin\\Debug\\file"; 
+                Class1.SaveGeoJSON(saveFilePath, geoJsonData);
+                Console.WriteLine($"Данные GeoJSON сохранены в {saveFilePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+        static void PrintGeoJSONData(Class1.GeoJSONData data)
+        {
+            Console.WriteLine($"Тип: {data.Type}");
+            Console.WriteLine($"Тип геометрии: {data.Geometry.Type}");
+            Console.WriteLine("Координаты:");
+            PrintCoordinates(data.Geometry.Coordinates);
+        }
+
+        static void PrintCoordinates(object coordinates)
+        {
+            string jsonCoordinates = JsonConvert.SerializeObject(coordinates, Formatting.Indented);
+            Console.WriteLine(jsonCoordinates);
+        }
+    }
+}
